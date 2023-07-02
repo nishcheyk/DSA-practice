1115d218f019cd2783a45d318eaaccf99276c03c
nishchey khajuria <nishcheycapture2014@gmail.com>
2023-07-02 16:50:23 +0530

    12-JUL-23

---
+++ stack/linklist stack.cpp
@@ -0,0 +1,62 @@
+#include<iostream>
+using namespace std;
+struct node
+{
+    int data;
+    node* next;
+    node(int d){
+        data=d;
+        next=NULL;
+    }
+};
+node* top=NULL;
+void push(int data )
+{   node* n= new node(data);
+    if(top==NULL){
+        n->next=NULL;
+    }
+    else
+        n->next=top;
+    top=n;
+}
+void pop(){
+
+    if(top==NULL)
+    cout<<"\nunderflow\n";
+    else{
+        node* temp=top;
+        top=temp->next;
+        free (temp);
+    }
+}
+void display(){
+    if(top==NULL)
+    cout<<"\nstack is empty";
+    else{
+        node* temp=top;
+        do
+        {
+              cout<<temp->data<<"--> ";
+            temp=temp->next;
+        } while (temp->next!=NULL);
+
+        cout<<temp->data<<"-->NULL";
+    }
+    cout<<"\n";
+}
+
+int main(){
+    push(5);
+push(10);
+display();
+push(11);
+push(12);
+display();
+cout<<"after del";
+pop();
+pop();
+display();
+pop();
+pop();
+display();
+}
